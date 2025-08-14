d0ab7bfa89918e70639f9ecb0e2747b38175bc1f
oracleclod939 <oraclecloud939@gmail.com>
2025-08-14 23:30:50 +0200

    adding first files

--- variables.tf
+++ variables.tf
@@ -4,9 +4,9 @@ variable "base_image_path" {
   default     = "/vstorage/IMG/windows-base/windows-template.sysprepped.qcow2"
 }
 
-variable "vm_name" { type = string; default = "autowin" }
-variable "pool"    { type = string; default = "windows-base" }
-variable "memory_mb" { type = number; default = 4096 }
-variable "vcpu" { type = number; default = 2 }
-variable "admin_password" { type = string; default = "Tu4n0nn4!" }
-variable "libvirt_uri" { type = string; default = "quemu:///system"}# da personalizzare
+variable "vm_name" { type = string, default = "autowin" }
+variable "pool"    { type = string, default = "windows-base" }
+variable "memory_mb" { type = number, default = 4096 }
+variable "vcpu" { type = number, default = 2 }
+variable "admin_password" { type = string, default = "Tu4n0nn4!" }
+variable "libvirt_uri" { type = string, default = "quemu:///system"}# da personalizzare
